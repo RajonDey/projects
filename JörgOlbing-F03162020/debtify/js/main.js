ef7aeebe59e993ba9c155dfad58f6635aa2d0296
RajonDey <rcirclebd@gmail.com>
2020-03-20 17:52:08 +0600

    Issues fix

--- JörgOlbing-F03162020/debtify/js/main.js
+++ JörgOlbing-F03162020/debtify/js/main.js
@@ -23,5 +23,14 @@ function stickyHeaderHandler() {
 	}
 }
 
+// FOR TOGGLE MENU
+function classToggle() {
+  const navs = document.querySelectorAll('.Navbar__Items')
+  
+  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+}
+
+document.querySelector('.Navbar__Link-toggle')
+  .addEventListener('click', classToggle);
 
 
